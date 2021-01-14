4494c5e53a4a10c4b3937df2313624f6ab0ac053
SeanMombo <Sean@mombourquette.com>
2021-01-13 21:04:16 -0500

    connected frontend search to flask with flask_wtf can now paste in a thread url and get the users who replied

--- twauth-web.py
+++ twauth-web.py
@@ -1,12 +1,19 @@
 from __future__ import unicode_literals
 import os
 from flask import Flask, render_template, request, url_for, redirect, session
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired as Required
+
 import tweepy
 import oauth2 as oauth
 import urllib.request
 import urllib.parse
 import urllib.error
 import json
+
+
 
 app = Flask(__name__)
 app.static_folder = 'static'
@@ -31,10 +38,12 @@ show_user_url = 'https://api.twitter.com/2/users/by'
 # APP_CONSUMER_SECRET = 'API_Secret_from_Twitter'
 
 app.config.from_pyfile('config.cfg', silent=True)
-
-
 oauth_store = {}
 
+
+class searchTweetForm(FlaskForm):
+    thread_url = StringField('Thread URL', validators=[Required()])
+    submit = SubmitField('Submit')
 
 @app.route('/')
 def hello():
@@ -68,26 +77,27 @@ def callback():
 def grid():
     # captain_mrs 2x2 thread
     # https://twitter.com/captain_mrs/status/1346473885789655041?s=20
-
-    if request.method == 'POST':
-
+    
+    form = searchTweetForm();
+    if request.method == 'POST' and form.validate_on_submit():
+        print(form.thread_url.data)
         token, token_secret = session['token']
         auth = tweepy.OAuthHandler(app.config['APP_CONSUMER_KEY'], app.config['APP_CONSUMER_SECRET'], app.config['CALLBACK'])
         auth.set_access_token(token, token_secret)
         api = tweepy.API(auth, parser=tweepy.parsers.JSONParser(), wait_on_rate_limit=True)
+        # api = tweepy.API(auth, wait_on_rate_limit_notify=True)
 
-        tweet_url = 'https://twitter.com/captain_mrs/status/1346473885789655041?s=20'
+        tweet_url = 'https://twitter.com/eigenrobot/status/1349276374113034242'
+        tweet_url = form.thread_url.data
         tweet_id = tweet_url.split('/')[-1].split('?')[0]
-        screen_name = 'captain_mrs'
+        screen_name = tweet_url.split('/')[-3]
         q_str = 'to:' + screen_name + ' conversation_id:' + tweet_id
 
         _max_queries = 5
-        n = 3000
+        n = 500
         ct = 1
 
-        tweets = tweet_batch = api.search(q=q_str, since_id=tweet_id, count=n, result_type='mixed', include_entities=True)
-        tweets = tweets['statuses']
-        tweet_batch = tweet_batch['statuses']
+        tweets = tweet_batch = api.search(q=q_str, since_id=tweet_id, count=n, result_type='mixed', include_entities=True)['statuses']
 
         #batch the search requests to get around 100 limit
         while len(tweets) < n and ct < _max_queries:
@@ -97,10 +107,9 @@ def grid():
                 if a['id'] < max_id:
                     max_id = a['id']
 
-            tweet_batch = api.search(q=q_str, since_id=tweet_id, count=n - len(tweets), result_type='mixed', include_entities=True, max_id=max_id)['statuses']         
+            tweet_batch = api.search(q=q_str, since_id=tweet_id, count=n - len(tweets), result_type='mixed', include_entities=True, max_id=max_id)['statuses']   
             tweets.extend(tweet_batch)
             ct += 1
-
 
 
         lis = []
@@ -111,22 +120,33 @@ def grid():
             cur_id = t['user']['id']
             if reply_id == tweet_id and cur_id not in lis:
                 screen_names.append(t['user']['screen_name'])
-                pfp_urls.append(t['user']['profile_image_url_https'])
+                
+                pfpImg = t['user']['profile_image_url_https'];
+                imgType = pfpImg[-4:]
+                pfpImg2 = pfpImg[:-11]
+                pfpImg2 += imgType
+                print(pfpImg2)
+                pfp_urls.append(pfpImg2)
                 lis.append(cur_id)
 
+        # for i in range(50):
+        #     screen_names.append('seanmombo');
+        #     pfp_urls.append('https://pbs.twimg.com/profile_images/1343923119791239169/JXx5YF6I.png')
+
+
         # print(screen_names)
+        # with open('./static/names.txt') as f:
+        #     names = f.read().splitlines()
 
-        with open('./static/names.txt') as f:
-            names = f.read().splitlines()
-
-        count = 0;
-        for n in names:
-            if n in screen_names:
-                count+=1
+        # count = 0;
+        # for n in names:
+        #     if n in screen_names:
+        #         count+=1
         # print(count, len(names))
-        return render_template('grid.html', pfp_urls=pfp_urls, access_token_url=access_token_url) 
 
-    return render_template('grid.html', data=[], access_token_url=access_token_url)
+        return render_template('grid.html', pfp_urls=pfp_urls, form=form, access_token_url=access_token_url) 
+
+    return render_template('grid.html', data=[], form=form, access_token_url=access_token_url)
 
 
 @app.errorhandler(500)
